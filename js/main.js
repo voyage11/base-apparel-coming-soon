0dfdda48457397e5c3638e03edc28f9aafc4bec6
Ricky <rickychea11@gmail.com>
2020-04-20 17:33:12 +0800

    - removed log

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 var isFormSubmitted = localStorage.getItem("isFormSubmitted");
 var isValidEmail = localStorage.getItem("isValidEmail");
 var email = localStorage.getItem("email");
-if(isFormSubmitted === "true" && isValidEmail === "false") {
-  console.log("email: " + email);
+
+if (isFormSubmitted === "true" && isValidEmail === "false") {
   $(".error").show();
   $(".icon-error").show();
   $("input").val(email);
@@ -12,22 +12,22 @@ if(isFormSubmitted === "true" && isValidEmail === "false") {
   $(".icon-error").hide();
 }
 
-$("button").click(function( event ) {
+$("button").click(function(event) {
   localStorage.isFormSubmitted = true;
-  if(validateEmail($("input").val())) {
+  if (validateEmail($("input").val())) {
     $(".error").hide();
     $(".icon-error").hide();
-    localStorage.isValidEmail  = true;
+    localStorage.isValidEmail = true;
     alert("Email submitted successfully!");
   } else {
     $(".error").show();
     $(".icon-error").show();
-    localStorage.isValidEmail  = false;
+    localStorage.isValidEmail = false;
     localStorage.email = $("input").val();
   }
 });
 
 function validateEmail(email) {
-    var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(String(email).toLowerCase());
+  var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(String(email).toLowerCase());
 }
